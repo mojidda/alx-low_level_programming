ade25a4fe69c3938bc66f00bf81a20ff184ae766
mojidda <muhammadjiddah09@gmail.com>
2023-02-18 16:15:18 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints upper case
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alx;
+
+	for (alx = 'a' ; alx <= 'z' ; alx++)
+		putchar(alx);
+	for (alx = 'A' ; alx <= 'Z' ; alx++)
+		putchar(alx);
+	putchar('\n');
+	return (0);
+}	
+	
