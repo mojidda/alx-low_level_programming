01c836b555480dec00b91ce10052e27695584e4e
mojidda <muhammadjiddah09@gmail.com>
2023-02-27 04:05:50 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes value point 98
+ * as parameter that views 98
+ * @n: value
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
